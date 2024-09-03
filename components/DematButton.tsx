47f0ed1fcf86d56b6daafea5c4aaef65ffb04baa
vivek-panchal <vivek.nsut@gmail.com>
2024-09-03 10:19:16 +0530

    refactor: Add external links to DematButton and TopNavbar components

--- components/DematButton.tsx
+++ components/DematButton.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 export default function DematButton() {
     return (
         <div className='flex justify-center py-3'>
-            <button className='rounded-full bg-[#33A34D] text-white px-4 py-3 w-56'>
-                Open Demat Account
-            </button>
+            <a href="https://indmoney.onelink.me/RmHC/gbvfnhdx" target="_blank" rel="noopener noreferrer">
+                <button className='rounded-full bg-[#33A34D] text-white px-4 py-3 w-56'>
+                    Open Demat Account
+                </button>
+            </a>
         </div>
     )
 }
--- components/TopNavbar.tsx
+++ components/TopNavbar.tsx
@@ -6,11 +6,17 @@ export default function TopNavbar() {
   return (
     <nav className='flex justify-between p-4 border-b'>
       <div>
-        <Image src="/logo.png" width={50} height={50} alt="logo" />
+          <a href="https://www.indmoney.com/" target="_blank" rel="noopener noreferrer">
+            <Image src="/logo.png" width={50} height={50} alt="logo" />
+          </a>
       </div>
       <div className='flex gap-2 items-center'>
         <div>
-            <button className='px-3 py-1 rounded-2xl border border-[#33A34D] text-[#33A34D]'>Log In</button>
+            <a href="https://www.indmoney.com/login" target="_blank" rel="noopener noreferrer">
+                <button className='px-3 py-1 rounded-2xl border border-[#33A34D] text-[#33A34D]'>
+                    Log In
+                </button>
+            </a>
         </div>
         <div className='flex items-center gap-3'>
         <Headset color='grey' className='mr-3'/>
